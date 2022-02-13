58cf445bc97b29a6b10f45aac7514575995fdd40
Valentin <85406438+valkata033@users.noreply.github.com>
2022-02-13 17:33:26 +0200

    Add files via upload

---
+++ 02.Conditions - Lab/02.AreaOfFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string type = Console.ReadLine();
+            if (type == "square")
+            {
+                double sideOfSquare = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{sideOfSquare * sideOfSquare:f3}");
+            }
+            else if (type == "rectangle")
+            {
+                double sideOfRectangle1 = double.Parse(Console.ReadLine());
+                double sideOfRectangle2 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{sideOfRectangle1 * sideOfRectangle2:f3}");
+            }
+            else if (type == "circle")
+            {
+                double radiusOfCircle = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{Math.PI * radiusOfCircle * radiusOfCircle:f3}");
+            }
+            else if (type == "triangle")
+            {
+                double sideLength = double.Parse(Console.ReadLine());
+                double heightLength = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{sideLength * heightLength / 2:f3}");
+            }
+
+
+
+        }
+    }
+}
---
+++ 02.Conditions - Lab/07.ToyShop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double priceOfHoliday = double.Parse(Console.ReadLine());
+            int numberOfPuzzels = int.Parse(Console.ReadLine());
+            int numberOfDolls = int.Parse(Console.ReadLine());
+            int numberOfBears = int.Parse(Console.ReadLine());
+            int numberOfMinions = int.Parse(Console.ReadLine());
+            int numberOfTrucks = int.Parse(Console.ReadLine());
+
+            double totalIncome = numberOfPuzzels * 2.60 + numberOfDolls * 3 + numberOfBears * 4.10 + numberOfMinions * 8.20 + numberOfTrucks * 2;
+
+            int numOfToys = numberOfBears + numberOfDolls + numberOfMinions + numberOfPuzzels + numberOfTrucks;
+
+            if (numOfToys >= 50)
+            {
+                totalIncome = totalIncome - totalIncome * 0.25;
+                
+            }
+            totalIncome = totalIncome - totalIncome * 0.10;
+            if (totalIncome >= priceOfHoliday)
+            {
+                Console.WriteLine($"Yes! {totalIncome - priceOfHoliday:f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {priceOfHoliday - totalIncome:f2} lv needed.");
+            }
+            
+        }
+    }
+}
